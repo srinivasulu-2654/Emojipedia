862475f3bb4f2244d788e6c5967f929b1ce15726
Srinivasulu <srinuvasulucsv@gmail.com>
2023-05-22 20:15:26 +0530

    Intial commit

---
+++ src/emojipedia.js
@@ -0,0 +1,47 @@
+const emojipedia = [
+    {
+      id: 1,
+      emoji: "💪",
+      name: "Tense Biceps",
+      meaning:
+        "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+    },
+    {
+      id: 2,
+      emoji: "🙏",
+      name: "Person With Folded Hands",
+      meaning:
+        "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+    },
+    {
+      id: 3,
+      emoji: "🤣",
+      name: "Rolling On The Floor, Laughing",
+      meaning:
+        "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+    },
+    {
+      id: 4,
+      emoji: "😍",
+      name: "Smiling Face With Heart-Eyes",
+      meaning:
+        "A smile of love is shown by the 😍 emoji, which beams with happiness with its heart-shaped eyes. It conveys intense love and affection for a person, place or even an object."
+    },
+    {
+      id: 5,
+      emoji: "😘",
+      name: " Face Throwing a Kiss",
+      meaning:
+        "Kiss emoji 😘 is an expression of affection. It conveys friendly or romantic sympathy and is often used to express tenderness or as a flirtatious signal."
+    },
+    {
+      id: 6,
+      emoji: "😇",
+      name: "Smiling Face With Halo",
+      meaning:
+        "A heavenly smile is shown by the 😇 halo emoji, which symbolizes innocence and goodness. People use it to express their good deeds, loving behavior or exemplary attitude."
+    }
+  ];
+  
+  export default emojipedia;
+  
